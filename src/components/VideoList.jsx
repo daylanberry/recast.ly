4febc0ec65048d93cfd5efbc9bda46ab67bb938e
daylanberry <daylanberry@gmail.com>
2019-09-16 21:02:28 -0700

    created map function

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,19 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
+import VideoListEntry from './VideoListEntry.js';
+var VideoList = (props) => (
+  // <div className="video-list">
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // </div>
+
+
+  props.videos.map(function(video){
+    return <div><h5><em>{video.title}</em></h5></div>
+  });
+
+
 );
 
 // PropTypes tell other developers what `props` a component expects
