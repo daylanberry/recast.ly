6e8e5e8cc10c9dad849d157c1117b21a1c0d9d52
Daylan <daylanberry@gmail.com>
2019-09-21 21:10:25 -0700

    Created Search capabilities

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 var Search = ({handleSearchInputChange}) => (
   <div className="search-bar form-inline">
     <input className="form-control" type="text"
-    onChange={(e) => handleSearchInputChange(e.target.value}
+    onChange={(e) => handleSearchInputChange(e.target.value)}
     />
     <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
